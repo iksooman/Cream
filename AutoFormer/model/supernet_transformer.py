e27a15fe660e2c551bfd135fff398e775fe05a1c
Iksoo Shin <iksooman@naver.com>
2023-01-10 15:36:09 +0900

    SuperNet Architecture (Convolution and Attention)

--- AutoFormer/model/supernet_transformer.py
+++ AutoFormer/model/supernet_transformer.py
@@ -7,9 +7,13 @@ from model.module.Linear_super import LinearSuper
 from model.module.layernorm_super import LayerNormSuper
 from model.module.multihead_super import AttentionSuper
 from model.module.embedding_super import PatchembedSuper
+from model.module.mbconv_super import DynamicMBConvLayer
 from model.utils import trunc_normal_
 from model.utils import DropPath
 import numpy as np
+
+from numba import vectorize, njit
+
 
 def gelu(x: torch.Tensor) -> torch.Tensor:
     if hasattr(torch.nn.functional, 'gelu'):
@@ -35,8 +39,33 @@ class Vision_TransformerSuper(nn.Module):
         self.num_classes = num_classes
         self.pre_norm=pre_norm
         self.scale=scale
-        self.patch_embed_super = PatchembedSuper(img_size=img_size, patch_size=patch_size,
-                                                 in_chans=in_chans, embed_dim=embed_dim)
+        self.in_chans=in_chans
+
+        self.ops_econv    = ['conv', 'conv', 'conv', 'conv']
+        self.out_ch_econv = [24, 48, 96, 192]
+        self.stride_econv = [2, 2, 2, 1]
+        self.kernel_econv = [3, 3, 3, 3]
+
+        def create_earlyconv(prev_out, out_ch_econv, stride_econv, kernel_econv):
+            econv_list = []
+            for e_ops, e_out, e_st, e_kernel in zip(self.ops_econv, self.out_ch_econv, self.stride_econv, kernel_econv):
+                if e_ops == 'conv':
+                    for ops_econv in [nn.Conv2d(prev_out, e_out, e_kernel, stride=e_st, padding=(e_kernel-1)//2, bias=False, groups=1), nn.BatchNorm2d(e_out), nn.ReLU()]:
+                        econv_list.append(ops_econv)
+                elif e_ops == 'max':
+                    for ops_econv in [nn.Conv2d(prev_out, e_out, 1, stride=1, padding=0, bias=False, groups=1), nn.BatchNorm2d(e_out), nn.ReLU(), nn.MaxPool2d(2, 2)]:
+                        econv_list.append(ops_econv)
+                prev_out = e_out
+            return nn.Sequential(*econv_list)
+        self.early_conv = create_earlyconv(self.in_chans, self.out_ch_econv, self.stride_econv, self.kernel_econv)
+
+        img_down = 1
+        for each_stride in self.stride_econv:
+            img_down *= each_stride
+
+        self.patch_embed_super = PatchembedSuper(img_size=img_size//img_down, patch_size=patch_size//img_down,
+                                                 in_chans=self.out_ch_econv[-1], embed_dim=embed_dim)
+                                                 # in_chans=in_chans, embed_dim=embed_dim)
         self.gp = gp
 
         # configs for the sampled subTransformer
@@ -56,7 +85,7 @@ class Vision_TransformerSuper(nn.Module):
                                                        attn_drop=attn_drop_rate, drop_path=dpr[i],
                                                        pre_norm=pre_norm, scale=self.scale,
                                                        change_qkv=change_qkv, relative_position=relative_position,
-                                                       max_relative_position=max_relative_position))
+                                                       max_relative_position=max_relative_position, add_conv=True if i < 0 else False))
 
         # parameters for vision transformer
         num_patches = self.patch_embed_super.num_patches
@@ -107,6 +136,7 @@ class Vision_TransformerSuper(nn.Module):
         self.sample_dropout = calc_dropout(self.super_dropout, self.sample_embed_dim[0], self.super_embed_dim)
         self.patch_embed_super.set_sample_config(self.sample_embed_dim[0])
         self.sample_output_dim = [out_dim for out_dim in self.sample_embed_dim[1:]] + [self.sample_embed_dim[-1]]
+        self.sample_mul = config['mul']
         for i, blocks in enumerate(self.blocks):
             # not exceed sample layer number
             if i < self.sample_layer_num:
@@ -118,7 +148,9 @@ class Vision_TransformerSuper(nn.Module):
                                         sample_num_heads=self.sample_num_heads[i],
                                         sample_dropout=sample_dropout,
                                         sample_out_dim=self.sample_output_dim[i],
-                                        sample_attn_dropout=sample_attn_dropout)
+                                        sample_attn_dropout=sample_attn_dropout,
+                                        sample_mul=self.sample_mul[i],
+                                        )
             # exceeds sample layer number
             else:
                 blocks.set_sample_config(is_identity_layer=True)
@@ -127,8 +159,18 @@ class Vision_TransformerSuper(nn.Module):
         self.head.set_sample_config(self.sample_embed_dim[-1], self.num_classes)
 
     def get_sampled_params_numel(self, config):
-        self.set_sample_config(config)
         numels = []
+
+        # early conv params
+        params = self.in_chans * self.out_ch_econv[0] * self.kernel_econv[0]
+        for i in range(len(self.out_ch_econv)-1):
+            params += self.out_ch_econv[i] * self.out_ch_econv[i+1] * self.kernel_econv[i] * self.kernel_econv[i]
+        #early conv batch params
+        for j in range(len(self.out_ch_econv)):
+            params += self.out_ch_econv[j] * 4
+        numels.append(params)
+
+        self.set_sample_config(config)
         for name, module in self.named_modules():
             if hasattr(module, 'calc_sampled_param_num'):
                 if name.split('.')[0] == 'blocks' and int(name.split('.')[1]) >= config['layer_num']:
@@ -136,6 +178,14 @@ class Vision_TransformerSuper(nn.Module):
                 numels.append(module.calc_sampled_param_num())
 
         return sum(numels) + self.sample_embed_dim[0]* (2 +self.patch_embed_super.num_patches)
+
+    def get_alphas(self, sigmoid=True):
+        return_list = []
+        for blocks in self.blocks:
+            return_list.append(blocks.get_alpha(sigmoid=sigmoid))
+
+        return return_list
+
     def get_complexity(self, sequence_length):
         total_flops = 0
         total_flops += self.patch_embed_super.get_complexity(sequence_length)
@@ -146,6 +196,7 @@ class Vision_TransformerSuper(nn.Module):
         return total_flops
     def forward_features(self, x):
         B = x.shape[0]
+        x = self.early_conv(x)
         x = self.patch_embed_super(x)
         cls_tokens = self.cls_token[..., :self.sample_embed_dim[0]].expand(B, -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
@@ -172,6 +223,76 @@ class Vision_TransformerSuper(nn.Module):
         return x
 
 
+class CustomizedMBConvLayer(nn.Module):
+
+    def __init__(self, channel, kernel_size, expand_ratio, class_emb=True):
+        super().__init__()
+        self.mbconv = DynamicMBConvLayer(in_channel_list=channel, out_channel_list=channel,
+                                         kernel_size_list=kernel_size, expand_ratio_list=expand_ratio,
+                                         use_se=True)
+        self.class_emb = class_emb
+
+    def forward(self, x):
+        residual = x
+
+        B, S, C = x.shape
+        H = int(math.sqrt(S))
+        if self.class_emb:
+            x = x[:, 1:]
+        x = x.transpose(1, 2)
+        x = torch.reshape(x, (B, C, H, H))
+        x = self.mbconv(x)
+        x = torch.reshape(x, (B, C, H*H))
+        x = x.transpose(1, 2)
+
+        if self.class_emb:
+            return torch.cat((x[:, :1, :]*0., x), dim=1) + residual
+        else:
+            return x + residual
+
+
+class LinearEvaluation(nn.Module):
+
+    def __init__(self, node_num):
+        super().__init__()
+        '''
+        self.fc1 = LinearSuper(super_in_dim=node_num, super_out_dim=node_num*2)
+        self.fc2 = LinearSuper(super_in_dim=node_num*2, super_out_dim=node_num*2)
+        self.fc3 = LinearSuper(super_in_dim=node_num*2, super_out_dim=node_num)
+        '''
+        self.fc1 = nn.Sequential(
+                        nn.Linear(node_num, max(128, node_num*4), bias=False),
+                        nn.BatchNorm1d(max(128, node_num*2)),
+                        nn.ReLU()
+                        )
+        self.fc2 = nn.Sequential(
+                        nn.Linear(max(128, node_num*4), max(128, node_num*4), bias=False),
+                        nn.BatchNorm1d(max(128, node_num*2)),
+                        nn.ReLU()
+                        )
+        self.fc3 = nn.Sequential(
+                        nn.Linear(max(128, node_num*4), node_num, bias=False),
+                        # nn.BatchNorm1d(node_num),
+                        )
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
+        return x
+
+
+class MaxLayer(nn.Module):
+    def __init__(self, dims=0):
+        super().__init__()
+        self.dims = dims
+
+    def forward(self, x, dim=None):
+        input_max, max_indices = torch.max(x, dim if dim is not None else self.dims)
+        return input_max
+
+
 class TransformerEncoderLayer(nn.Module):
     """Encoder layer block.
 
@@ -181,7 +302,7 @@ class TransformerEncoderLayer(nn.Module):
 
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, dropout=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, pre_norm=True, scale=False,
-                 relative_position=False, change_qkv=False, max_relative_position=14):
+                 relative_position=False, change_qkv=False, max_relative_position=14, add_conv=False):
         super().__init__()
 
         # the configs of super arch of the encoder, three dimension [embed_dim, mlp_ratio, and num_heads]
@@ -205,6 +326,13 @@ class TransformerEncoderLayer(nn.Module):
         self.sample_dropout = None
         self.sample_attn_dropout = None
 
+        self.add_conv = True
+        if self.add_conv:
+            print('Mixed Layer')
+            self.conv = CustomizedMBConvLayer(channel=dim, kernel_size=3, expand_ratio=6, class_emb=True)
+            self.alpha = torch.nn.parameter.Parameter(nn.init.constant_(torch.Tensor(1), 0.), requires_grad=True)
+            self.beta  = torch.nn.parameter.Parameter(nn.init.constant_(torch.Tensor(1), 0.), requires_grad=True)
+
         self.is_identity_layer = None
         self.attn = AttentionSuper(
             dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop,
@@ -222,7 +350,7 @@ class TransformerEncoderLayer(nn.Module):
         self.fc2 = LinearSuper(super_in_dim=self.super_ffn_embed_dim_this_layer, super_out_dim=self.super_embed_dim)
 
 
-    def set_sample_config(self, is_identity_layer, sample_embed_dim=None, sample_mlp_ratio=None, sample_num_heads=None, sample_dropout=None, sample_attn_dropout=None, sample_out_dim=None):
+    def set_sample_config(self, is_identity_layer, sample_embed_dim=None, sample_mlp_ratio=None, sample_num_heads=None, sample_dropout=None, sample_attn_dropout=None, sample_out_dim=None, sample_mul=None):
 
         if is_identity_layer:
             self.is_identity_layer = True
@@ -240,13 +368,25 @@ class TransformerEncoderLayer(nn.Module):
         self.sample_attn_dropout = sample_attn_dropout
         self.attn_layer_norm.set_sample_config(sample_embed_dim=self.sample_embed_dim)
 
-        self.attn.set_sample_config(sample_q_embed_dim=self.sample_num_heads_this_layer*64, sample_num_heads=self.sample_num_heads_this_layer, sample_in_embed_dim=self.sample_embed_dim)
+        self.sample_mul = sample_mul
+        self.attn.set_sample_config(sample_q_embed_dim=self.sample_num_heads_this_layer*64, sample_num_heads=self.sample_num_heads_this_layer, sample_in_embed_dim=self.sample_embed_dim, sample_mul=sample_mul)
 
         self.fc1.set_sample_config(sample_in_dim=self.sample_embed_dim, sample_out_dim=self.sample_ffn_embed_dim_this_layer)
         self.fc2.set_sample_config(sample_in_dim=self.sample_ffn_embed_dim_this_layer, sample_out_dim=self.sample_out_dim)
 
         self.ffn_layer_norm.set_sample_config(sample_embed_dim=self.sample_embed_dim)
 
+        if self.add_conv:
+            self.conv.mbconv.set_subnet_config(in_channel=self.sample_embed_dim)
+
+    def roundup(self, num):
+        return int(num) + 1 if (num - int(num)) >= 0.5 else int(num)
+
+    def get_alpha(self, sigmoid):
+        if sigmoid:
+            return torch.sigmoid(self.alpha)
+        else:
+            return self.alpha
 
     def forward(self, x):
         """
@@ -262,29 +402,54 @@ class TransformerEncoderLayer(nn.Module):
         # compute attn
         # start_time = time.time()
 
-        residual = x
-        x = self.maybe_layer_norm(self.attn_layer_norm, x, before=True)
-        x = self.attn(x)
-        x = F.dropout(x, p=self.sample_attn_dropout, training=self.training)
-        x = self.drop_path(x)
-        x = residual + x
-        x = self.maybe_layer_norm(self.attn_layer_norm, x, after=True)
-        # print("attn :", time.time() - start_time)
-        # compute the ffn
-        # start_time = time.time()
-        residual = x
-        x = self.maybe_layer_norm(self.ffn_layer_norm, x, before=True)
-        x = self.activation_fn(self.fc1(x))
-        x = F.dropout(x, p=self.sample_dropout, training=self.training)
-        x = self.fc2(x)
-        x = F.dropout(x, p=self.sample_dropout, training=self.training)
-        if self.scale:
-            x = x * (self.super_mlp_ratio / self.sample_mlp_ratio)
-        x = self.drop_path(x)
-        x = residual + x
-        x = self.maybe_layer_norm(self.ffn_layer_norm, x, after=True)
-        # print("ffn :", time.time() - start_time)
-        return x
+        sample_prob = torch.nn.functional.softmax(torch.concat([self.alpha, self.beta]))
+        if self.training:
+            sample_choice = [torch.bernoulli(sample_prob[0])]
+        else:
+            sample_choice = [self.roundup(sample_prob[0])]
+            print(sample_choice)
+
+        if sample_choice[0] > 0.5:
+            x_conv = self.conv(x)
+            b = 1. - sample_prob[0].detach()
+            c = sample_prob[0] + b
+            return x_conv * c
+
+        else:
+            residual = x
+            x = self.maybe_layer_norm(self.attn_layer_norm, x, before=True)
+            x = self.attn(x)
+            x = F.dropout(x, p=self.sample_attn_dropout, training=self.training)
+            x = self.drop_path(x)
+            x = residual + x
+            x = self.maybe_layer_norm(self.attn_layer_norm, x, after=True)
+            # print("attn :", time.time() - start_time)
+            # compute the ffn
+            # start_time = time.time()
+            residual = x
+            x = self.maybe_layer_norm(self.ffn_layer_norm, x, before=True)
+            x = self.activation_fn(self.fc1(x))
+            x = F.dropout(x, p=self.sample_dropout, training=self.training)
+            x = self.fc2(x)
+            x = F.dropout(x, p=self.sample_dropout, training=self.training)
+            if self.scale:
+                x = x * (self.super_mlp_ratio / self.sample_mlp_ratio)
+            x = self.drop_path(x)
+            x = residual + x
+            x = self.maybe_layer_norm(self.ffn_layer_norm, x, after=True)
+            # print("ffn :", time.time() - start_time)
+
+            b = sample_prob[1].detach()
+            c = 1. - sample_prob[1] + b
+            return x * c
+
+        '''
+        if self.add_conv:
+            a = torch.sigmoid(self.alpha)
+            return a*x  + (1.-a)*self.conv(x)
+        else:
+            return x
+        '''
 
     def maybe_layer_norm(self, layer_norm, x, before=False, after=False):
         assert before ^ after
